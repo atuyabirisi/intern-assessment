92fddf561fb383e27cbdedcad71ce70d699ed1b1
joykara <joykarani1@gmail.com>
2024-06-10 19:49:23 +0300

    set up theme

---
+++ payd-test/src/theme/theme.ts
@@ -0,0 +1,46 @@
+import { extendTheme } from '@chakra-ui/react';
+
+// Define custom colors
+const colors = {
+  brand: {
+    darkGreen: '#22543D', // Dark Green
+    amber: '#FFBF00', // Amber
+    white: '#FFFFFF', // White
+  },
+};
+
+// Create the theme with custom colors and settings
+const customTheme = extendTheme({
+    colors,
+    styles: {
+        global: {
+        'html, body': {
+            bg: 'brand.white',
+            fontFamily: 'Arial, sans-serif'
+        },
+        },
+    },
+    components: {
+        Input: {
+            baseStyle: {
+                focusBorderColor: 'brand.amber',
+            },
+        },
+        Button: {
+            baseStyle: {
+                padding: '1px',
+            },
+            variants: {
+                solid: {
+                    bg: 'brand.amber',
+                    color: 'brand.white',
+                    _hover: {
+                        bg: 'brand.darkGreen',
+                    },
+                },
+            },
+        },
+    }
+});
+
+export default customTheme;
