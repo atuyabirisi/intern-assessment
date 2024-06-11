b028fdccc4bfecafce40a90a70d19f4f85af0af9
joykara <joykarani1@gmail.com>
2024-06-11 13:42:56 +0300

    feat: Add Navbar and update App component

--- payd-test/src/App.tsx
+++ payd-test/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.css';
+import Navbar from './components/Navbar';
 import Posts from './components/Posts';
 
-function App() {
+const App: React.FC = () => {
+
   return (
     <>
-      <Posts />
+      <Navbar />
+      <Posts/>
     </>
   );
 }
